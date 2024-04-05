65f772c6b4d63e72caad2e8f7a99e33fa52c7833
eladkapuza <eladkapuza@gmail.com>
2024-04-05 19:46:50 +0300

    fixed port.js

--- js/port.js
+++ js/port.js
@@ -4,13 +4,13 @@ class Port extends Tile {
     this.type = type;
   }
 
-  Draw(refPos) {
-    image(
-      this.image,
-      this.pos.x + refPos.x + this.width / 2,
-      this.pos.y + refPos.y + this.width / 2,
-      this.width,
-      this.width
-    );
-  }
+  // Draw(refPos) {
+  //   image(
+  //     this.image,
+  //     this.pos.x + refPos.x + this.width / 2,
+  //     this.pos.y + refPos.y + this.width / 2,
+  //     this.width,
+  //     this.width
+  //   );
+  // }
 }
